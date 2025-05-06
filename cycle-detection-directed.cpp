2ea836d38d91fed287419a8c68091e6035b904df
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-06 08:37:51 +0530

    Update cycle-detection-directed.cpp

--- cycle-detection-directed.cpp
+++ cycle-detection-directed.cpp
@@ -14,8 +14,8 @@ bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<b
         }
         inRecursion[u] = false;
         return false;
-    }
-    bool isCyclic(int V, vector<vector<int>> &edges) {
+}
+bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         vector<vector<int>> adj(V);
         for(auto &i : edges) {
@@ -31,4 +31,40 @@ bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<b
             }
         }
         return false;
-    }
+}
+
+// *************************************** Detect cycle in directed graph - BFS - using Kahn's Algorithm ****************************************************************
+// if a graph is cyclic, topoSort is not possible.
+bool isCycleDGBFS(int V, vector<vector<int>> edges) {
+        vector<vector<int>> adj(V);
+        for(auto &i : edges) {
+                adj[i[0]].push_back(i[1]);
+        }
+        queue<int> q;
+        vector<int> indegree(V, 0);
+        for(int u = 0; u < V; u++) {
+                for(int &v : adj[u]) {
+                        indegree[v]++;
+                }
+        }
+        int count = 0;
+        for(int i = 0; i < V; i++) {
+                if(indegree[i] == 0) {
+                        q.push(i);
+                        count++;
+                }
+        }
+        while(!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for(int &v : adj[u]) {
+                        indegree[v]--;
+                        if(indegree[v] == 0) {
+                                q.push(v);
+                                count++;
+                        }
+                }
+        }
+        if(count == V) return false;
+        else return true;
+}
