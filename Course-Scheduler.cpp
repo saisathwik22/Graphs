203161b81f7da31467bdf223c8aac8d25c0356d0
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-08 09:00:48 +0530

    Update Course-Scheduler.cpp

--- Course-Scheduler.cpp
+++ Course-Scheduler.cpp
@@ -40,3 +40,37 @@ bool isCompleted(int N, vector<vector<int>> prereq) {
   }
   return topoSortCheck(adj, N, indegree);
 }
+
+// using DFS cycle Detection algo
+    bool isCycleDFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
+
+        for(int &v : adj[u]) {
+            if(!visited[v] && isCycleDFS(adj, v, visited, inRecursion)) {
+                return true;
+            }
+            else if(inRecursion[v] == true) {
+                return true;
+            }
+        }
+        inRecursion[u] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        for(auto &i : prerequisites) {
+            int a = i[0];
+            int b = i[1];
+            adj[b].push_back(a);
+        }
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
+
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion)) {
+                return false;
+            }
+        }
+        return true;
+    }
