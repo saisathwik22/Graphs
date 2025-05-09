20d1b6fc2440d4231320e37be1a0368abf7f878c
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-09 21:27:10 +0530

    Update Bipartite-Graph.cpp

--- Bipartite-Graph.cpp
+++ Bipartite-Graph.cpp
@@ -36,3 +36,44 @@ bool isBipartite(int V, vector<vector<int>> edges) {
   }
   return true;
 }
+
+// BFS Approach
+bool isiBipartiteBFS(vector<vector<int>>& adj, int currNode, vector<int>& color, int currNodeColor) {
+  queue<int> q;
+  q.push(currNode);
+  color[currNode] = currNodeColor;
+  while(!q.empty()) {
+    int U = q.front();
+    q.pop();
+    for(int &v : adj[U]) {
+      if(color[v] == color[U]) {
+        return false;
+      }
+      else if(color[v] == -1) {
+        color[v] = 1 - color[U];
+        q.push(v);
+      }
+    }
+  }
+  return true;
+}
+
+
+bool isBipartite(int V, vector<vector<int>> edges) {
+  unordered_map<int, vector<int>> adj;
+  for(int &v : edges) {
+    int u = i[0];
+    int v = i[1];
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  vector<int> color(V, -1);
+  for(int i = 0; i < V; i++) {
+    if(color[v] == -1) {
+      if(isBipartiteBFS(adj, i, color, 1) == false) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
