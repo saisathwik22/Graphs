63e0e017a2403319e541de5d74dec940565964e2
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-15 23:20:20 +0530

    JAVA code added.

--- cycle-detection-undirected.cpp
+++ cycle-detection-undirected.cpp
@@ -1,5 +1,6 @@
 // Cycle detection using DFS and BFS in Undirected Graph
-// ************************************* DFS*****************************88
+// ************************************************* C++ **************************************
+// DFS
 bool isCycleDFS(int u, vector<vector<int>> &adj, vector<bool> &visited, int parent)
 {
     
@@ -45,7 +46,7 @@ bool isCycle(int V, vector<vector<int>> &edges)
     return false;
 }
 
-// ***************************************************** BFS ****************************************************************
+//BFS 
 bool isCycleBFS(vector<vector<int>>& adj, int u, vector<bool>& visited) 
 {
         queue<pair<int, int>> q;
@@ -93,4 +94,78 @@ bool isCycle(int V, vector<vector<int>>& edges)
             }
         }
         return false;
+}
+
+// ************************************************ JAVA ************************************************************
+// DFS Aproach
+class Solution {
+    public boolean isCycleDFS(int u, List<List<Integer>> adj, boolean[] visited, int parent) {
+        visited[u] = true;
+        for(int v : adj.get(u)) {
+            if(!visited[v]) {
+                if(isCycleDFS(v, adj, visited, u)) return true;
+            } else if(v != parent) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        boolean[] visited = new boolean[V];
+        for(int u = 0; u < V; u++) {
+            if(!visited[u]) {
+                if(isCycleDFS(u, adj, visited, -1)) return true;
+            }
+        }
+        return false;
+    }
+}
+// BFS Approach
+class Solution {
+    public boolean isCycleBFS(List<List<Integer>> adj, int u, boolean[] visited) {
+        Queue<int[]>q = new LinkedList<>();
+        q.add(new int[]{u , -1});
+        visited[u] = true;
+        while(!q.isEmpty()) {
+            int[] pair = q.poll();
+            int node = pair[0];
+            int parent = pair[1];
+            for(int v : adj.get(node)) {
+                if(!visited[v]) {
+                    visited[v] = true;
+                    q.add(new int[]{v, node});
+                } else if(v != parent) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        boolean[] visited = new boolean[V];
+        for(int i = 0; i < V; i++) {
+            if(!visited[i]) {
+                if(isCycleBFS(adj, i, visited)) return true;
+            }
+        }
+        return false;
+    }
 }
