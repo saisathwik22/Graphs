32f20589faacb92d3ffa7f863f69bed4432c893f
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-15 23:27:08 +0530

    JAVA code added.

--- TopoSortDFS.cpp
+++ TopoSortDFS.cpp
@@ -3,6 +3,7 @@
 // we are using unordered_map to create adjacency list out of a 2d matrix edges.
 // returning a vector ans containing the topo sort order.
 
+// *********************************************** C++ ***************************************************
 void DFS(unoredered_map<int, vector<int>> adj, int u, vector<bool>& visited, stack<int>& s) {
   visited[u] = true;
   for(auto &v : adj[u]) {
@@ -33,4 +34,37 @@ vector<int> topoSortDFS(int V, vector<vector<int>>& edges) {
     s.pop();
   }
   return ans;
+}
+// ****************************************************** JAVA *****************************************************************
+class Solution {
+    public static void dfs(Map<Integer, List<Integer>> adj, int u, boolean[] visited, Stack<Integer> s) {
+        visited[u] = true;
+        for(int v : adj.getOrDefault(u, new ArrayList<>())) {
+            if(!visited[v]) {
+                dfs(adj, v, visited, s);
+            }
+        }
+        s.push(u);
+    }
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        boolean[] visited = new boolean[V];
+        Stack<Integer> s = new Stack<>();
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+        }
+        for(int i = 0; i < V; i++) {
+            if(!visited[i]) {
+                dfs(adj, i, visited, s);
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        while(!s.isEmpty()) {
+            result.add(s.pop());
+        }
+        return result;
+    }
 }
