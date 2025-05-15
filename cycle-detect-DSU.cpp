ae43a684a5ce570a576149b9b81872969e61e55c
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-15 18:45:15 +0530

    added java code also

--- cycle-detect-DSU.cpp
+++ cycle-detect-DSU.cpp
@@ -6,6 +6,7 @@
 // if parents of two nodes are found equal, then CYCLE exists
 // if you donot find any edge from graph such that the parents of those two nodes are equal --> cycle donot exist;
 
+// *******************************************   C++ *********************************************************************************
 vector<int> parent;
 vector<int> rank;
 int find(int x) {
@@ -45,4 +46,59 @@ int detectCycle(int X, vector<vector<int>> adj) {
     }
   }
   return false;
+}
+// *********************************************************************  JAVA *****************************************************************************************
+class Solution {
+    
+    int[] parent;
+    int[] rank;
+    
+    public int find(int x) {
+        if(parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+    
+    public void unionByRank(int x, int y) {
+        int xParent = find(x);
+        int yParent = find(y);
+        if(xParent == yParent) return;
+        if(rank[xParent] > rank[yParent]){
+            parent[yParent] = xParent;
+        }
+        else if(rank[xParent] < rank[yParent]) {
+            parent[xParent] = yParent;
+        }
+        else {
+            parent[xParent] = yParent;
+            rank[yParent]++;
+        }
+    }
+    
+    // Function to detect cycle using DSU in an undirected graph.
+    public int detectCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        parent = new int[V];
+        rank = new int[V];
+        
+        for(int i = 0; i < V; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+        
+        for(int u = 0; u < V; u++) {
+            for(int v : adj.get(u)) {
+                if(u < v) {
+                    if(find(u) == find(v)) {
+                        return 1;
+                    }
+                    else {
+                        unionByRank(u, v);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 }
