7cb04e13f67c15aa0ff5d9e2a0ef9f882bf4c255
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-15 22:48:07 +0530

    JAVA code added.

--- DFS&BFS.cpp
+++ DFS&BFS.cpp
@@ -1,5 +1,5 @@
+// ******************************************* C++ *****************************************************
 // result array return respective DFS and BFS traversal orders.
-
 void DFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<int>& result) {
   if(visited[u] == true) return;
   visited[u] = true;
@@ -26,6 +26,35 @@ void BFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<int>& re
         q.push(v);
         visited[v] = true;
         result.push_back(v);
+      }
+    }
+  }
+}
+// ****************************************************** JAVA *********************************************************
+// DFS
+public static void DFS(List<List<Integer>> adj, int u, boolean[] visited, List<Integer> result) {
+  if(visited[u]) return;
+  visited[u] = true;
+  result.add(u);
+  for(int v : adj.get(u)) {
+    if(!visited[v]) {
+      DFS(adj, v, visited, result);
+    }
+  }
+}
+// BFS
+public static void BFS(List<List<Integer>> adj, int u, boolean[] visited, List<Integer> result) {
+  Queue<Integer> q = new LinkedList<>();
+  q.add(u);
+  visited[u] = true;
+  result.add(u);
+  while(!q.isEmpty()) {
+    int curr = q.poll();
+    for(int v : adj.get(curr)) {
+      if(!visited[v]) {
+        q.add(v);
+        visited[v] = true;
+        result.add(v);
       }
     }
   }
