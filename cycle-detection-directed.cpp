b46fe26d0aa7b2ac7c76c550292eed282c576459
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-15 23:09:17 +0530

    JAVA code added.

--- cycle-detection-directed.cpp
+++ cycle-detection-directed.cpp
@@ -1,3 +1,4 @@
+// ********************************************** C++ ***********************************************************************************8
 // cycle detection in graph using DFS
 bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
         
@@ -67,4 +68,83 @@ bool isCycleDGBFS(int V, vector<vector<int>> edges) {
         }
         if(count == V) return false;
         else return true;
+}
+
+// ****************************************************** JAVA ************************************************************************
+// DFS Approach
+class Solution {
+    public boolean isCycleDFS(List<List<Integer>> adj, int u, boolean[] visited, boolean[] inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
+        
+        for(int v : adj.get(u)) {
+            if(!visited[v] && isCycleDFS(adj, v, visited, inRecursion)) {
+                return true;
+            } else if(inRecursion[v]) {
+                return true;
+            }
+        }
+        inRecursion[u] = false;
+        return false;
+    }
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+        boolean[] visited = new boolean[V];
+        boolean[] inRecursion = new boolean[V];
+        
+        for(int i = 0; i < V; i++) {
+            if(!visited[i]) {
+                if(isCycleDFS(adj, i, visited, inRecursion)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// BFS approach - kahns algo
+class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+        int[] indegree = new int[V];
+        for(int u = 0; u < V; u++) {
+            for(int v : adj.get(u)) {
+                indegree[v]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        for(int i = 0; i < V; i++) {
+            if(indegree[i] == 0) {
+                count++;
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()) {
+            int u = q.poll();
+            for(int v : adj.get(u)) {
+                indegree[v]--;
+                if(indegree[v] == 0) {
+                    q.add(v);
+                    count++;
+                }
+            }
+        }
+        return count != V;
+    }
 }
