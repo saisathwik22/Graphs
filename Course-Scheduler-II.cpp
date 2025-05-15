cc8c82fea8d76cc1485edc259753c99b47da27af
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-15 22:42:33 +0530

    JAVA code added.

--- Course-Scheduler-II.cpp
+++ Course-Scheduler-II.cpp
@@ -1,6 +1,6 @@
 // Question same as Course Scheduler I, but here we have to return order of taking courses.
 // Order of taking courses, is ntg but TopoSort Order.
-
+// ****************************************** C++ **********************************************************
 // Approach 1 : Using Topo Sort BFS - Kahn's Algo
 vector<int> topoSortBFS(unordered_map<int, vector<int>> adj, int n, vector<int>& indegree) {
   queue<int> q;
@@ -84,4 +84,88 @@ vector<int> findOrder(int n, vector<vector<int>> prerequisites) {
     s.pop();
   }
   return ans;
+}
+// ********************************************************* JAVA ***************************************************
+// Approach - BFS (kahns Algo) 
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int[] indegree = new int[numCourses];
+        for(int[] pre : prerequisites) {
+            int a = pre[0];
+            int b = pre[1];
+            adj.computeIfAbsent(b, k -> new ArrayList<>()).add(a);
+            indegree[a]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.offer(i);
+                result.add(i);
+            }
+        }
+        while(!q.isEmpty()) {
+            int u = q.poll();
+            for(int v : adj.getOrDefault(u, new ArrayList<>())) {
+                indegree[v]--;
+                if(indegree[v] == 0) {
+                    q.offer(v);
+                    result.add(v);
+                }
+            }
+        }
+        if(result.size() != numCourses) return new int[0];
+        int[] order = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            order[i] = result.get(i);
+        }
+        return order;
+    }
+}
+
+// Approach DFS
+class Solution {
+    boolean hasCycle;
+    public void dfs(Map<Integer, List<Integer>> adj, int u, boolean[] visited, boolean[] inRecursion, Stack<Integer> s) {
+        visited[u] = true;
+        inRecursion[u] = true;
+
+        for(int v : adj.getOrDefault(u, new ArrayList<>()))  {
+            if(inRecursion[v]) {
+                hasCycle = true;
+                return;
+            }
+            if(!visited[v]) {
+                dfs(adj, v, visited, inRecursion, s);
+                if(hasCycle) return;
+            }
+        }
+        inRecursion[u] = false;
+        s.push(u);
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int[] pre : prerequisites) {
+            adj.computeIfAbsent(pre[1], k -> new ArrayList<>()).add(pre[0]);
+        }
+
+        boolean[] visited = new boolean[numCourses];
+        boolean[] inRecursion = new boolean[numCourses];
+        Stack<Integer> s = new Stack<>();
+        hasCycle = false;
+
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) {
+                dfs(adj, i, visited, inRecursion, s);
+                if(hasCycle) return new int[0];
+            }
+        }
+        int[] result = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            result[i] = s.pop();
+        }
+        return result;
+    }
 }
