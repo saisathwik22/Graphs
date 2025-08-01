a154859dd18eb8d4530cf7854302d37480f4d622
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-08-01 09:50:13 +0530

    JAVA code added.

--- number-of-islands.java
+++ number-of-islands.java
@@ -1,3 +1,8 @@
+// Paytm, Amazon, Microsoft, Samsung, Snapdeal, Citrix, DE Shaw, Ola cabs, Visa, Linkedin, Opera, Streamoid Technologies, 
+
+// DFS 
+// TC : O(m*n)
+
 class Solution {
     public void DFS(char[][] grid, int i, int j) {
         int m = grid.length;
@@ -25,6 +30,50 @@ class Solution {
                 if(grid[i][j] == '1') {
                     count++;
                     DFS(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+// BFS
+// TC : O(m*n) SC : O(m*n)
+class Solution {
+    int m, n;
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private boolean isSafe(char[][] grid, int i, int j) {
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1';
+    }
+    public void BFS(char[][] grid, int i, int j, Queue<int[]> q) {
+        q.offer(new int[]{i, j});
+        grid[i][j] = '$';
+
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+
+            for(int[] dir : directions) {
+                int i_ = curr[0] + dir[0];
+                int j_ = curr[1] + dir[1];
+
+                if(isSafe(grid, i_, j_)) {
+                    q.offer(new int[]{i_, j_});
+                    grid[i_][j_] = '$';
+                }
+            }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        int count = 0;
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    BFS(grid, i, j, q);
+                    count++;
                 }
             }
         }
