179f7380ecfcb183a1cddfc8f44c0aba32ae2143
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-08-08 11:25:21 +0530

    JAVA code added.

---
+++ time-needed-to-inform-all-employees.java
@@ -0,0 +1,32 @@
+// leetcode 1376 - AMAZON
+
+// DFS
+class Solution {
+
+    public int maxTime = 0;
+    public void DFS(Map<Integer, List<Integer>> adj, int[] informTime, int curr_employee, int curr_time) {
+        maxTime = Math.max(maxTime, curr_time);
+
+        for(int employee : adj.get(curr_employee)) {
+            DFS(adj, informTime, employee, curr_time + informTime[curr_employee]);
+        }
+    }
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        for(int i = 0; i < manager.length; i++) {
+            int employee_i = i;
+            int manager_i = manager[i];
+
+            if(manager_i != -1) {
+                adj.computeIfAbsent(manager_i, k -> new ArrayList<>()).add(employee_i);
+            }
+        }
+
+        DFS(adj, informTime, headID, 0);
+
+        return maxTime;
+    }
+}
